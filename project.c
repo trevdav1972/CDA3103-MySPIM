a758af91497c97a35fdd641b9d4fe0fc68617752
Hapa149 <hapa1491@gmail.com>
2026-04-17 20:43:56 -0400

    PC_Update and control signals

--- project.c
+++ project.c
@@ -42,7 +42,6 @@ void ALU(unsigned A,unsigned B,char ALUControl,unsigned *ALUresult,char *Zero) /
   if (ALUControl == 4) { 
     // Get ALUResult
     *ALUresult = A & B;
-
   }
 
   // Perform Or operation
@@ -55,14 +54,12 @@ void ALU(unsigned A,unsigned B,char ALUControl,unsigned *ALUresult,char *Zero) /
   if (ALUControl == 6) { 
     // Get ALUResult
     *ALUresult << 16;
-
   }
 
   // Not
   if (ALUControl == 7) { 
     // Get ALUResult
     *ALUresult ~A;
-
   }
 
   // If ALUResult == 0 return Var ZERO = 1, if anything else var is zero
@@ -97,116 +94,69 @@ void instruction_partition(unsigned instruction, unsigned *op, unsigned *r1,unsi
   unsigned shamt = (instruction << 21) >> 27;  //instruction [10-6]    idek if we need this for this assignment, since it doesnt exists in the 
                                                                         //datapath signals in spimcore.c but i figured id add it and we can assign it to an actual value if needed
   *funct = (instruction >> 26) << 26;  //instruction [5-0]
-  *offset = (instruction << 16) >> 16;
+  *offset = (instruction << 16) >> 16;	//instruction [15-0]
 }
 
 /* instruction decode */
 /* 15 Points */
-int instruction_decode(unsigned op, struct_controls *controls) //Trevor 
-{
-  switch op {
-    case 0:  //R-Type
-      controls -> RegDst = 1;
-      controls -> Jump = 0;
-      controls -> Branch = 0;
-      controls -> MemRead = 0;
-      controls -> MemToReg = 0;
-      controls -> ALUOp = ;
-      controls -> MemWrite = 0;
-      controls -> ALUSrc = ;
-      controls -> RegWrite = 1;
-      break;
-    case 2:  //j  jump
-      controls -> RegDst = 0;
-      controls -> Jump = 1;
-      controls -> Branch = 0;
-      controls -> MemRead = 0;
-      controls -> MemToReg = 0;
-      controls -> ALUOp = 0;
-      controls -> MemWrite = 0;
-      controls -> ALUSrc = 0;
-      controls -> RegWrite = 0;
-      break;
-    case 4:  //beq  branch on equal
-      controls -> RegDst = 0;
-      controls -> Jump = ;
-      controls -> Branch = 1;
-      controls -> MemRead = 0;
-      controls -> MemToReg = 0;
-      controls -> ALUOp = ;
-      controls -> MemWrite = ;
-      controls -> ALUSrc = ;
-      controls -> RegWrite = ;
-      break;
-    case 8:  //addi  add immediate
-      controls -> RegDst = ;
-      controls -> Jump = ;
-      controls -> Branch = ;
-      controls -> MemRead = ;
-      controls -> MemToReg = ;
-      controls -> ALUOp = ;
-      controls -> MemWrite = ;
-      controls -> ALUSrc = ;
-      controls -> RegWrite = ;
-      break;
-    case 10:  //slti  set less than immediate
-      controls -> RegDst = ;
-      controls -> Jump = ;
-      controls -> Branch = ;
-      controls -> MemRead = ;
-      controls -> MemToReg = ;
-      controls -> ALUOp = ;
-      controls -> MemWrite = ;
-      controls -> ALUSrc = ;
-      controls -> RegWrite = ;
-      break;
-    case 11:  //sltiu  set less than immediate upper
-      controls -> RegDst = ;
-      controls -> Jump = ;
-      controls -> Branch = ;
-      controls -> MemRead = ;
-      controls -> MemToReg = ;
-      controls -> ALUOp = ;
-      controls -> MemWrite = ;
-      controls -> ALUSrc = ;
-      controls -> RegWrite = ;
-      break;
-    case 15:  //lui  load upper immediate
-      controls -> RegDst = ;
-      controls -> Jump = ;
-      controls -> Branch = ;
-      controls -> MemRead = ;
-      controls -> MemToReg = ;
-      controls -> ALUOp = ;
-      controls -> MemWrite = ;
-      controls -> ALUSrc = ;
-      controls -> RegWrite = ;
-      break;
-    case 35:  //lw  load word
-      controls -> RegDst = ;
-      controls -> Jump = ;
-      controls -> Branch = ;
-      controls -> MemRead = ;
-      controls -> MemToReg = ;
-      controls -> ALUOp = ;
-      controls -> MemWrite = ;
-      controls -> ALUSrc = ;
-      controls -> RegWrite = ;
-      break;
-    case 43:  //sw  store word
-      controls -> RegDst = ;
-      controls -> Jump = ;
-      controls -> Branch = ;
-      controls -> MemRead = ;
-      controls -> MemToReg = ;
-      controls -> ALUOp = ;
-      controls -> MemWrite = ;
-      controls -> ALUSrc = ;
-      controls -> RegWrite = ;
-      break;
-    default:
-      return 1;
-  }
+int instruction_decode(unsigned op, struct_controls *controls) //Trevor		I'm being deadass I finished doing this whole function before reading
+{																		 // that you put your name here so uhhhhhh	
+	//set base controls to zero
+	controls -> RegDst = 0;
+    cntrols -> Jump = 0;
+	controls -> Branch = 0;
+    controls -> MemRead = 0;
+    controls -> MemToReg = 0;
+    controls -> ALUOp = 0;
+    controls -> MemWrite = 0;
+    controls -> ALUSrc = 0;
+    controls -> RegWrite = 0;
+
+	//assign proper controls per opcode
+	switch op {
+		case 0:  //R-Type
+			controls -> RegDst = 1;
+	        controls -> ALUOp = 7;
+    	    controls -> RegWrite = 1;
+	        break;
+		case 2:  //j  jump
+			controls -> Jump = 1;
+		    break;
+		case 4:  //beq  branch on equal
+		    controls -> Branch = 1;
+		    break;
+		case 8:  //addi  add immediate
+		    controls -> RegDst = 1;
+		    controls -> ALUSrc = 1;
+		    controls -> RegWrite = 1;
+		    break;
+		case 10:  //slti  set less than immediate
+		    controls -> ALUOp = 2;
+		    controls -> ALUSrc = 1;
+		    controls -> RegWrite = 1;
+		    break;
+		case 11:  //sltiu  set less than immediate unsigned
+		    controls -> ALUOp = 3;
+		    controls -> ALUSrc = 1;
+		    controls -> RegWrite = 1;
+		    break;
+		case 15:  //lui  load upper immediate
+		    controls -> ALUSrc = 1;
+		    controls -> RegWrite = 1;
+		    break;
+		case 35:  //lw  load word
+		    controls -> MemRead = 1;
+		    controls -> MemToReg = 1;
+		    controls -> ALUSrc = 1;
+		    controls -> RegWrite = 1;
+			break;
+		case 43:  //sw  store word
+		    controls -> MemWrite = 1;
+		    controls -> ALUSrc = 1;
+			break;
+		default:
+			return 1;
+	}
 	return 0;
 }
 
@@ -221,9 +171,10 @@ void read_register(unsigned r1,unsigned r2,unsigned *Reg,unsigned *data1,unsigne
 /* Sign Extend */
 /* 10 Points */
 void sign_extend(unsigned offset,unsigned *extended_value) {
-	if (offset <= 0b1000000000000000) {
+	//if number is positive, no additional logic needed
+	if (offset <= 0x8000) {
 		*extended_value = offset;
-	} else {
+	} else {	//if number is negative, add leading ones to retain value
 		*extended_value = offset + 0xFFFF0000;
 	}
 }
@@ -254,6 +205,12 @@ void write_register(unsigned r2,unsigned r3,unsigned memdata,unsigned ALUresult,
 /* 10 Points */
 void PC_update(unsigned jsec,unsigned extended_value,char Branch,char Jump,char
 Zero,unsigned *PC) {
-
+	*PC += 4;
+	if (Jump) {
+		*PC = (*PC & 0xF0000000) + (jsec << 2);	//first 4 bits of pc, jsec, 2 bits of 0
+	} else {
+		if (Branch && Zero)
+			*PC += (extended_value << 2);
+	}
 }
 
